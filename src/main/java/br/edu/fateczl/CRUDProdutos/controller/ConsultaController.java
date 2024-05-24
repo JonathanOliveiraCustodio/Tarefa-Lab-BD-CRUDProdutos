db478a947286846283f96a9765bb133d1f00e3de
Jonathan Custodio <126304158+JonathanOliveiraCustodio@users.noreply.github.com>
2024-05-24 01:50:39 -0300

    Implementação do Relatório PDF

--- src/main/java/br/edu/fateczl/CRUDProdutos/controller/ConsultaController.java
+++ src/main/java/br/edu/fateczl/CRUDProdutos/controller/ConsultaController.java
@@ -1,19 +1,36 @@
 package br.edu.fateczl.CRUDProdutos.controller;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
 import br.edu.fateczl.CRUDProdutos.model.Produto;
 import br.edu.fateczl.CRUDProdutos.persistence.GenericDao;
 import br.edu.fateczl.CRUDProdutos.persistence.ProdutoDao;
-
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.JasperRunManager;
+import net.sf.jasperreports.engine.util.JRLoader;
 
 @Controller
 public class ConsultaController {
@@ -24,44 +41,77 @@ public class ConsultaController {
     @Autowired
     ProdutoDao pDao;
 
-
     @RequestMapping(name = "consulta", value = "/consulta", method = RequestMethod.GET)
-	public ModelAndView indexGet(@RequestParam Map<String, String> allRequestParam, ModelMap model) {
-
-		return new ModelAndView("consulta");
-	}
+    public ModelAndView consultaGet(@RequestParam Map<String, String> allRequestParam, ModelMap model) {
+        return new ModelAndView("consulta");
+    }
 
     @RequestMapping(name = "consulta", value = "/consulta", method = RequestMethod.POST)
-    public ModelAndView dependentePost(@RequestParam("valorEntrada") int valorEntrada, ModelMap model) {
-        return realizarConsulta(valorEntrada, model);
+    public ModelAndView processForm(@RequestParam("valorEntrada") int valorEntrada, @RequestParam("action") String action, ModelMap model) {
+        
+    	if ("Consultar".equals(action)) {
+            return realizarConsulta(valorEntrada, model);
+        } else 
+        	if ("Gerar".equals(action)) {
+            return new ModelAndView("forward:/produtoRelatorio?valorEntrada=" + valorEntrada);
+        }
+        return new ModelAndView("consulta", model);
     }
 
     private ModelAndView realizarConsulta(int valorEntrada, ModelMap model) {
         String erro = "";
         List<Produto> produtos = null;
-        
         int qtdEstoque = 0;
-        
         try {
             produtos = listarProdutos(valorEntrada);
             qtdEstoque = calcularQtdProdutos(valorEntrada);
         } catch (SQLException | ClassNotFoundException e) {
             erro = e.getMessage();
         }
-        
         model.addAttribute("erro", erro);
         model.addAttribute("produtos", produtos);
-        model.addAttribute("qtdEstoque", qtdEstoque); 
-        
+        model.addAttribute("qtdEstoque", qtdEstoque);
         return new ModelAndView("consulta", model);
+    }
+
+    @SuppressWarnings({ "rawtypes" })
+    @RequestMapping(name = "produtoRelatorio", value = "/produtoRelatorio", method = RequestMethod.POST)
+    public ResponseEntity produtoPost(@RequestParam Map<String, String> allRequestParam) {
+        String erro = "";
+        Map<String, Object> paramRelatorio = new HashMap<String, Object>();
+        paramRelatorio.put("quantidade", allRequestParam.get("valorEntrada"));
+        byte[] bytes = null;
+
+        InputStreamResource resource = null;
+        HttpStatus status = null;
+        HttpHeaders header = new HttpHeaders();
+
+        try {
+            Connection c = gDao.getConnection();
+            File arquivo = ResourceUtils.getFile("classpath:reports/RelatorioProdutos.jasper");
+            JasperReport report = (JasperReport) JRLoader.loadObjectFromFile(arquivo.getAbsolutePath());
+            bytes = JasperRunManager.runReportToPdf(report, paramRelatorio, c);
+        } catch (ClassNotFoundException | SQLException | FileNotFoundException | JRException e) {
+            e.printStackTrace();
+            erro = e.getMessage();
+            status = HttpStatus.BAD_REQUEST;
+        } finally {
+            if (erro.equals("")) {
+                InputStream inputStream = new ByteArrayInputStream(bytes);
+                resource = new InputStreamResource(inputStream);
+                header.setContentLength(bytes.length);
+                header.setContentType(MediaType.APPLICATION_PDF);
+                status = HttpStatus.OK;
+            }
+        }
+        return new ResponseEntity<>(resource, header, status);
     }
 
     private List<Produto> listarProdutos(int valor) throws SQLException, ClassNotFoundException {
         return pDao.listarProduto(valor);
     }
-    
+
     private int calcularQtdProdutos(int QtdProdutos) throws SQLException, ClassNotFoundException {
         return pDao.calcularQtdProdutos(QtdProdutos);
     }
 }
-
--- src/main/webapp/WEB-INF/view/consulta.jsp
+++ src/main/webapp/WEB-INF/view/consulta.jsp
@@ -1,6 +1,6 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
+    pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
 <!DOCTYPE html>
 
@@ -9,84 +9,88 @@
 <meta charset="UTF-8">
 <title>Consulta de Produtos</title>
 <link rel="stylesheet" href="./css/styles.css">
+<script>
+    function handleFormSubmit(form, event) {
+        if (event.submitter.id === 'gerar') {
+            form.target = '_blank';
+            form.action = 'produtoRelatorio';
+        } else {
+            form.target = '';
+            form.action = 'consulta';
+        }
+    }
+</script>
 </head>
 <body>
-	<div>
-		<jsp:include page="menu.jsp" />
-	</div>
+    <div>
+        <jsp:include page="menu.jsp" />
+    </div>
 
-	<br />
+    <br />
 
-	<div align="center" class="container">
-		<form action="consulta" method="post">
-			<p class="title">
-				<b>Consulta</b>
-			</p>
+    <div align="center" class="container">
+        <form action="consulta" method="post" onsubmit="handleFormSubmit(this, event);">
+            <p class="title">
+                <b>Consulta</b>
+            </p>
 
-			<table>
-				<tr>
-					<td colspan="4" style="text-align: center;"><label
-						for="codigo"
-						style="display: inline-block; width: 30%; text-align: right;">Valor
-							de Entrada:</label> <input class="input_data_id" type="number" min="0"
-						step="1" id="valorEntrada" name="valorEntrada"
-						placeholder="Valor Entrada" required
-						style="display: inline-block; width: 30%;"> <input
-						type="submit" id="botao" name="botao" value="Consultar"
-						style="display: inline-block;"></td>
-				</tr>
-			</table>
-		</form>
+            <table>
+                <tr>
+                    <td colspan="4" style="text-align: center;">
+                        <label for="codigo" style="display: inline-block; width: 30%; text-align: right;">
+                            Valor de Entrada:
+                        </label>
+                        <input class="input_data_id" type="number" min="0" step="1" id="valorEntrada" name="valorEntrada" placeholder="Valor Entrada" required style="display: inline-block; width: 30%;" value="${param.valorEntrada != null ? param.valorEntrada : ''}">
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="4" style="text-align: center;">
+                        <input type="submit" id="consultar" name="action" value="Consultar" style="display: inline-block;">
+                        <input type="submit" id="gerar" name="action" value="Gerar" style="display: inline-block;">
+                    </td>
+                </tr>
+            </table>
+        </form>
 
+        <br /> <br />
 
+        <h2>Resultado da Consulta:</h2>
 
-		<h2>Resultado da Consulta:</h2>
+        <div align="center">
+            <c:if test="${not empty erro}">
+                <p style="color: red;">
+                    <c:out value="${erro}" />
+                </p>
+            </c:if>
+        </div>
 
-		<div align="center">
-			<c:if test="${not empty erro}">
-				<p style="color: red;">
-					<c:out value="${erro }" />
-				</p>
-			</c:if>
-		</div>
+        <br />
 
-		<br />
-
-		<div align="center">
-			<c:if test="${not empty produtos}">
-				<table class="table_round">
-					<thead>
-						<tr>
-							<th>Código</th>
-							<th>Nome</th>
-							<th>Qtd Estoque</th>
-
-						</tr>
-					</thead>
-					<tbody>
-						<c:forEach var="produto" items="${produtos}">
-							<tr>
-
-								<td><c:out value="${produto.codigo }" /></td>
-								<td><c:out value="${produto.nome }" /></td>
-								<td><c:out value="${produto.qtdEstoque }" /></td>
-
-							</tr>
-						</c:forEach>
-					</tbody>
-				</table>
-				<div align="center">
-					<div align="center">
-						<div align="center">
-							<h3>Quantidade de Produtos Abaixo do valor de "${param.valorEntrada}" é igual a
-								 ${qtdEstoque}</h3>
-						</div>
-
-					</div>
-
-				</div>
-			</c:if>
-		</div>
-	</div>
+        <div align="center">
+            <c:if test="${not empty produtos}">
+                <table class="table_round">
+                    <thead>
+                        <tr>
+                            <th>Código</th>
+                            <th>Nome</th>
+                            <th>Qtd Estoque</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <c:forEach var="produto" items="${produtos}">
+                            <tr>
+                                <td><c:out value="${produto.codigo}" /></td>
+                                <td><c:out value="${produto.nome}" /></td>
+                                <td><c:out value="${produto.qtdEstoque}" /></td>
+                            </tr>
+                        </c:forEach>
+                    </tbody>
+                </table>
+                <div align="center">
+                    <h3>Quantidade de Produtos Abaixo do valor de "${param.valorEntrada}" é igual a ${qtdEstoque}</h3>
+                </div>
+            </c:if>
+        </div>
+    </div>
 </body>
 </html>
